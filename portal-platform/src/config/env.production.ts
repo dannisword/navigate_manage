06c6f9929f506a92d7e51949d3b2029120d13979
dannis <dannis.word@gmail.com>
2025-08-22 15:33:36 +0800

    fix env

---
+++ portal-platform/src/config/env.production.ts
@@ -0,0 +1,4 @@
+module.exports = {
+  APP_TITLE: "My App (Prod)",
+  API_URL: "https://api.example.com",
+};
--- portal-platform/webpack.config.js
+++ portal-platform/webpack.config.js
@@ -6,87 +6,96 @@ const { ElementPlusResolver } = require("unplugin-vue-components/resolvers"); //
 
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/main.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "postcss-loader", // tailwind 加這個
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"], // 如果有用 scss/sass
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
-        options: { appendTsSuffixTo: [/\.vue$/] },
-      },
-    ],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    // 使用 HtmlWebpackPlugin
-    new HtmlWebpackPlugin({
-      // 指定模板檔案
-      template: "./public/index.html",
-      // 輸出的檔案名稱
-      filename: "index.html",
-      // 自動將所有打包的資源注入
-      inject: "body",
-    }),
-    // 自動匯入 Vue API（不用每次 import { ref, reactive } from 'vue'）
-    AutoImport({
-      imports: ["vue", "vue-router", "vue-i18n", "@vueuse/core"],
-      dts: "src/auto-imports.d.ts", // 產生型別檔，方便 TS 使用
-      eslintrc: {
-        enabled: true, // 會自動生成 .eslintrc-auto-import.json
-      },
-    }),
+module.exports = (mode) => {
+  console.log(mode);
+  
+  // const envConfig = require(`./config/env.${
+  //   env.production ? "production" : "development"
+  // }.js`);
 
-    // 自動註冊元件
-    Components({
-      dirs: ["src/components"], // 預設就是這個
-      extensions: ["vue"],
-      deep: true,
-      dts: "src/components.d.ts",
-      resolvers: [
-        ElementPlusResolver(), // 如果用 Element Plus
+  return {
+    mode: mode,//env.production ? "production" : "development",
+    entry: "./src/main.ts",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+        {
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            "css-loader",
+            "postcss-loader", // tailwind 加這個
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"], // 如果有用 scss/sass
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.ts$/,
+          loader: "ts-loader",
+          exclude: /node_modules/,
+          options: { appendTsSuffixTo: [/\.vue$/] },
+        },
       ],
-    }),
-  ],
-  resolve: {
-    extensions: [".ts", ".vue", ".js", ".json"],
-    alias: {
-      "@": path.resolve(__dirname, "src"),
     },
-  },
-  devServer: {
-    static: {
-      // 這裡你可以指向一個空的 dist 資料夾，讓 Webpack 自動生成
-      directory: path.join(__dirname, "dist"),
+    plugins: [
+      new VueLoaderPlugin(),
+      // 使用 HtmlWebpackPlugin
+      new HtmlWebpackPlugin({
+        // 指定模板檔案
+        template: "./public/index.html",
+        // 輸出的檔案名稱
+        filename: "index.html",
+        title: "Navigate", // 👈 這裡設定 title
+        // 自動將所有打包的資源注入
+        inject: "body",
+      }),
+      // 自動匯入 Vue API（不用每次 import { ref, reactive } from 'vue'）
+      AutoImport({
+        imports: ["vue", "vue-router", "vue-i18n", "@vueuse/core"],
+        dts: "src/auto-imports.d.ts", // 產生型別檔，方便 TS 使用
+        eslintrc: {
+          enabled: true, // 會自動生成 .eslintrc-auto-import.json
+        },
+      }),
+
+      // 自動註冊元件
+      Components({
+        dirs: ["src/components"], // 預設就是這個
+        extensions: ["vue"],
+        deep: true,
+        dts: "src/components.d.ts",
+        resolvers: [
+          ElementPlusResolver(), // 如果用 Element Plus
+        ],
+      }),
+    ],
+    resolve: {
+      extensions: [".ts", ".vue", ".js", ".json"],
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-    open: true,
-    hot: true,
-  },
+    devServer: {
+      static: {
+        // 這裡你可以指向一個空的 dist 資料夾，讓 Webpack 自動生成
+        directory: path.join(__dirname, "dist"),
+      },
+      open: true,
+      hot: true,
+    },
+  };
 };
